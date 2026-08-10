d92374719af616689b6684905e9922a996644b82
jhonatanCLMobi <jhonatan@mobi.lat>
2026-08-10 01:56:25 -0400

    refactor: simplify body parsing in ResponseModel

--- lib/core/networking/response_model.dart
+++ lib/core/networking/response_model.dart
@@ -19,20 +19,6 @@ class ResponseModel<T> {
     required this.body,
   });
 
-  static T _parseBody<T>(dynamic body) {
-    final data = body['data'];
-
-    try {
-      if (body['success'] && data == null) {
-        return {'message': body['message']} as T;
-      }
-
-      return data as T;
-    } catch (e) {
-      rethrow;
-    }
-  }
-
   factory ResponseModel.fromJson(JSON json) {
     return ResponseModel(
       headers: json['headers'] == null
@@ -40,7 +26,7 @@ class ResponseModel<T> {
           : ResponseHeadersModel.fromJson(
               json['headers'] as JSON,
             ),
-      body: _parseBody<T>(json['body']),
+      body: json['body'] as T,
     );
   }
   final ResponseHeadersModel? headers;
